151c4c1fb2c7be2f5bd7638e06a4f7c180ed29ab
GM <dinhhungGM>
2021-05-18 23:08:12 +0700

    combine to one file

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 3000
 const path = require('path')
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
