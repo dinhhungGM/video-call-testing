27e77dc88e5241caf17c8e3054ad640c7c3c6639
GM <dinhhungGM>
2021-05-19 14:02:38 +0700

    try fix sth

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const server = require('http').createServer(app)
+const cors = require("cors");
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+})
 const { v4: uuidV4 } = require('uuid')
 const PORT = process.env.PORT || 3000
 const path = require('path')
 
-
+app.use(cors())
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
 
